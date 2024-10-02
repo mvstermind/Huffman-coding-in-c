44c761ed97a9cc8f17af8219b6c9a911e2600f9a
mvstermind <zablockidawid1@gmail.com>
2024-10-02 22:02:49 +0200

    coding

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ struct HuffmanTreeNode{
 }; 
 typedef struct HuffmanTreeNode TreeNode;
 
+
 struct HuffmanTreeNodes {
     unsigned size;
 
@@ -22,3 +23,33 @@ struct HuffmanTreeNodes {
     
 };
 typedef struct HuffmanTreeNodes TreeNodes;
+
+
+TreeNode* newNode(char data, unsigned freq){
+    TreeNode* temp = (TreeNode*)malloc(sizeof(TreeNode));
+
+    temp->left = temp->right = NULL;
+    temp->data = data;
+    temp->freq = freq;
+
+    return temp;
+};
+
+TreeNodes* createTreeNodes(unsigned capacity){
+    TreeNodes* treeNodes = (TreeNodes*)malloc(sizeof(TreeNodes));
+
+    treeNodes->size = 0;
+    treeNodes->capacity = capacity;
+    treeNodes->array = (TreeNode**)malloc(
+        treeNodes->capacity * sizeof(TreeNode*));
+
+    return treeNodes;
+}
+
+
+void swapNodes(TreeNode** a, TreeNode b){
+    TreeNode* t = *a;
+    *a = *b;
+    *b = t;
+}
+
